5cf9ecc45d1ae8ab93f6d4be7b6f63ce00ed53b3
Luis Segovia <luiissegovia@icloud.com>
2022-02-15 14:17:12 -0700

    added the labels and the variables

--- multiply/ViewController.swift
+++ multiply/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+
+    
+        
+    
+    @IBOutlet weak var displayProduct: UILabel!
+ 
+    @IBOutlet weak var number1: UITextField!
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    
+    
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func bmwi8(_ sender: Any) {
+        
+        var first1 = number1.text
+        
+        var first12 = number2.text
+        
+        
+        
+    }
+    
 }
 
