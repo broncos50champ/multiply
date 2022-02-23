f1bd689ca0aa71a16149d38524b7fc8489d44b47
Luis Segovia <luiissegovia@icloud.com>
2022-02-23 13:45:56 -0700

    save final product

--- multiply/ViewController.swift
+++ multiply/ViewController.swift
@@ -25,8 +25,27 @@ class ViewController: UIViewController {
 override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    assignbackground()
     }
 
+    
+    func assignbackground()  {
+        let background = UIImage (named: "car")
+        var imageView  : UIImageView
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+    
+    
+    
+    
+    
+    
     
     @IBAction func bmwi8(_ sender: Any) {
         
